9f84e2012cd48e5bab22394127e1a943c396170c
OrangeX4 <318483724@qq.com>
2022-11-16 18:46:47 +0800

    Add CSV function

--- output/__init__.py
+++ output/__init__.py
@@ -37,18 +37,7 @@ class Output:
         '''
         diff_list = []
         for cluster_name in self.clusters:
-            cluster_dict = self.clusters[cluster_name].cluster
-            for file1 in cluster_dict["files"]:
-                for file2 in cluster_dict["files"]:
-                    if file1 < file2:
-                        diff_list.append({
-                            "cluster_name": cluster_name,
-                            "file1": file1,
-                            "file2": file2,
-                            "auto": cluster_dict["diff"][file1][file2]["auto"],
-                            "manual": cluster_dict["diff"][file1][file2]["manual"],
-                            "logic": cluster_dict["diff"][file1][file2]["logic"]
-                        })
+            diff_list += self.cluster_to_diff_list(cluster_name)
         return diff_list
 
     def cluster_to_diff_list(self, cluster_name: str) -> list:
@@ -78,27 +67,44 @@ class Output:
                     })
         return diff_list
 
-    def save_diff_list_to_csv(self) -> None:
+
+    def diff_list_to_csv(self, mode="auto") -> Dict:
+        diff_list = self.clusters_to_diff_list()
+        equal_csv_content = "file1,file2"
+        unknown_csv_content = "file1,file2"
+        inequal_csv_content = "file1,file2"
+        for diff in diff_list:
+            _file1 = f'input/{diff["cluster_name"]}/{diff["file1"]}'
+            _file2 = f'input/{diff["cluster_name"]}/{diff["file2"]}'
+            if diff[mode] == "equiv":
+                equal_csv_content += f'\n{_file1},{_file2}'
+            elif diff[mode] == "unknown":
+                unknown_csv_content += f'\n{_file1},{_file2}'
+            elif diff[mode] == "unequiv":
+                inequal_csv_content += f'\n{_file1},{_file2}'
+        return {
+            "equal_csv_content": equal_csv_content,
+            "unknown_csv_content": unknown_csv_content,
+            "inequal_csv_content": inequal_csv_content
+        }
+        
+    
+    def save_diff_list_to_csv(self, mode="auto") -> None:
         '''
         保存到 path/output/equal.csv 和 path/output/inequal.csv
         格式为 input/cluster_name/file1,input/cluster_name/file2
         其中是否 equal 只取决于 auto == 'equiv' 与 auto == 'unequiv'
         '''
-        diff_list = self.clusters_to_diff_list()
         equal_csv_path = os.path.abspath(
             os.path.join(self.path, "output", "equal.csv"))
+        unknown_csv_path = os.path.abspath(
+            os.path.join(self.path, "output", "unknown.csv"))
         inequal_csv_path = os.path.abspath(
             os.path.join(self.path, "output", "inequal.csv"))
-        equal_csv_content = "file1,file2"
-        inequal_csv_content = "file1,file2"
-        for diff in diff_list:
-            _file1 = f'input/{diff["cluster_name"]}/{diff["file1"]}'
-            _file2 = f'input/{diff["cluster_name"]}/{diff["file2"]}'
-            if diff["auto"] == "equiv":
-                equal_csv_content += f'\n{_file1},{_file2}'
-            elif diff["auto"] == "unequiv":
-                inequal_csv_content += f'\n{_file1},{_file2}'
+        content_dict = self.diff_list_to_csv(mode)
         with open(equal_csv_path, 'w') as f:
-            f.write(equal_csv_content)
+            f.write(content_dict["equal_csv_content"])
+        with open(unknown_csv_path, 'w') as f:
+            f.write(content_dict["unknown_csv_content"])
         with open(inequal_csv_path, 'w') as f:
-            f.write(inequal_csv_content)
+            f.write(content_dict["inequal_csv_content"])
---
+++ server/__init__.py
@@ -0,0 +1,147 @@
+if __name__ == '__main__':
+    import sys
+    sys.path.append("..")
+from flask import Flask, request
+from typing import Dict
+from input import Input
+from output import Output
+from paracomp import Paracomp
+from cluster import Cluster
+
+# Flask
+app = Flask(__name__)
+# data 路径
+path = "../data"
+
+# 加载
+print('读取输入中...')
+input = Input(path, from_clusters=True)
+# Clusters
+clusters: Dict[str, Cluster] = input.clusters
+# 输出模块
+output = Output(path, clusters)
+
+
+def get_cluster(cluster_name) -> Dict:
+    '''
+    获取 cluster dict 的函数, 并且更新 diff_list
+    '''
+    cluster = clusters[cluster_name]
+    cluster_dict = cluster.cluster
+    cluster_dict['diff_list'] = output.cluster_to_diff_list(cluster_name)
+    return cluster_dict
+
+
+# 进行并行比较
+print('加载比较器...')
+paracomp = Paracomp(path, input)
+# for cluster_name in paracomp.get_cluster_names():
+#     # 如果是加载的则跳过
+#     if input.clusters[cluster_name].cluster['is_loaded']:
+#         continue
+#     paracomp.run(cluster_name)
+
+
+@app.after_request
+def after_request(resp):
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Method'] = '*'
+    resp.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+    return resp
+
+
+@app.route("/")
+def handle_root():
+    return "<p>backend of autodiff</p>"
+
+
+@app.route("/clusters", methods=['GET'])
+def handle_clusters():
+    result = {}
+    for cluster_name in clusters:
+        result[cluster_name] = get_cluster(cluster_name)
+    return result
+
+
+@app.route("/csv", methods=['GET'])
+def handle_csv():
+    return output.diff_list_to_csv("logic")
+
+
+@app.route("/cluster/<cluster_name>", methods=['GET'])
+def handle_cluster(cluster_name):
+    if cluster_name in clusters:
+        return get_cluster(cluster_name)
+    else:
+        return {
+            'status': 'error',
+            'message': 'No such cluster.'
+        }
+
+
+@app.route("/run", methods=['POST'])
+def handle_run():
+    '''
+    { "cluster_name": cluster_name }
+    '''
+    data = request.json
+    if data is not None and 'cluster_name' in data:
+        if data['cluster_name'] in clusters:
+            paracomp.run(data['cluster_name'])
+            # 保存结果
+            output.save_clusters()
+            return get_cluster(data['cluster_name'])
+        else:
+            return {
+                'status': 'error',
+                'message': 'No such cluster.'
+            }
+    else:
+        return {
+            'status': 'error',
+            'message': 'There is no "cluster_name" in request.'
+        }
+
+
+@app.route("/update", methods=['POST'])
+def handle_update():
+    '''
+    {
+        "cluster_name": cluster_name,
+        "file1": file1,
+        "file2": file2,
+        "manual": manual,
+    }
+    '''
+    data = request.json
+    if data is not None:
+        if 'cluster_name' in data \
+                and 'file1' in data \
+                and 'file2' in data \
+                and 'manual' in data:
+            if data['cluster_name'] in clusters:
+                cluster = clusters[data['cluster_name']]
+                cluster.set_manual(
+                    data['file1'], data['file2'], data['manual'])
+                # 保存结果
+                output.save_clusters()
+                return get_cluster(data['cluster_name'])
+            else:
+                return {
+                    'status': 'error',
+                    'message': 'No such cluster.'
+                }
+        else:
+            return {
+                'status': 'error',
+                'message': 'There is no neccessary key in request.'
+            }
+    else:
+        return {
+            'status': 'error',
+            'message': 'Data is empty.'
+        }
+
+
+if __name__ == '__main__':
+    app.run(debug=False, port=7376, host='0.0.0.0')
